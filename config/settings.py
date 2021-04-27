222f7cac6582eb2346ef1219714a148bc8e30737
akanjimm <akanjimm@gmail.com>
2021-04-27 22:30:32 +0100

    second commit

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,10 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# This redirects the users to the homepage after action
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+# Sends email to users
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
