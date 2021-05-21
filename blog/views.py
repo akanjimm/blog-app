1dc7898586d9bf02a44fd34483f9166408768fce
akanjimm <akanjimm@gmail.com>
2021-05-21 19:59:38 +0100

    adding permissions and authorization functionalities for create, edit and delete views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,38 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
 
 # Create your views here.
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body']
+        
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -11,19 +40,3 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-
-class BlogCreateView(CreateView):
-		model = Post
-		template_name = 'post_new.html'
-		fields = ['title', 'author', 'body']
-
-
-class BlogUpdateView(UpdateView):
-    model = Post
-    template_name = 'post_edit.html'
-    fields = ['title', 'body']
-
-class BlogDeleteView(DeleteView):
-    model = Post
-    template_name = 'post_delete.html'
-    success_url = reverse_lazy('home')
