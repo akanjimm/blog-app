f858623f0fb5c2e8f05091a275b75f7fd06d303d
akanjimm <akanjimm@gmail.com>
2021-05-22 06:21:59 +0100

    added comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from blog import views
 from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/comment/', views.post_comment, name='post_comment'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+
+from django.views import generic
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Post
+
 from django.urls import reverse_lazy
+
+from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class BlogCreateView(LoginRequiredMixin, CreateView):
@@ -38,5 +44,28 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 class BlogDetailView(DetailView):
+    form_class = CommentForm
     model = Post
     template_name = 'post_detail.html'
+
+def post_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'post_comment.html', {'form': form})
+
+
+
+
+'''class BlogCommentView(generic.CreateView):
+    form_class = CommentForm
+    success_url = reverse_lazy('post_detail', pk=Post.pk)
+    template_name = 'post_comment.html'
+    '''
