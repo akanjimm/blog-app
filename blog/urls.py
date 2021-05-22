0991fdae78867a9853b05361c928a346dcf30342
akanjimm <akanjimm@gmail.com>
2021-05-22 07:44:25 +0100

    added permission for comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blog import views
+from django.contrib.auth.decorators import login_required
 from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
-    path('post/<int:pk>/comment/', views.post_comment, name='post_comment'),
+    path('post/<int:pk>/comment/', login_required(views.post_comment), name='post_comment'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
